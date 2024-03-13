97c454882caa91b8af493c2f2c90eabf68b700d5
杉岡直美 <naominiusagi@sugiokanaominoMacBook-Air.local>
2024-03-13 22:13:57 +0900

    change session to username in dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -26,7 +26,7 @@
                 <h1 class="h3">The Company</h1>
             </a>
             <div class="navbar-nav">
-                <span class="navbar-text"><?= $_SESSION['full_name']?></span>
+                <span class="navbar-text"><?= $_SESSION['username']?></span>
                 <form action="../actions/logout.php" method="post" class="d-flex ms-2">
                     <button type="submit" class="text-danger bg-transparent border-0">Log out</button>
                 </form>
